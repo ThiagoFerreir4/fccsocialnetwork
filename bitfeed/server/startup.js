cb0ebfd59a4378969c99e9639ba90cf5af458097
Darryl Pargeter <darrylpargeter@gmail.com>
2015-07-17 02:13:10 +0100

    login paged changed, github auth added

---
+++ bitfeed/server/startup.js
@@ -0,0 +1,16 @@
+Meteor.startup(function(){
+});
+
+ServiceConfiguration.configurations.upsert({
+    service: 'github'
+},{
+    $set: {
+        clientId:'964566d992da7810a502', 
+        secret:'11e3951810577e8070c99fefdd69a5ad2d09d7c8',
+        loginStyle: 'popup'
+    }
+}
+                                          )
+
+
+
